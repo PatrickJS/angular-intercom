19430234477c8c71437491fd9e7a33d7f255e663
gdi2290 <github@gdi2290.com>
2013-11-05 20:24:23 -0800

    add async loading intercom script

--- angular-intercom.js
+++ angular-intercom.js
@@ -1,11 +1,57 @@
 ;(function(module, angular, window, document, undefined) {
+'use strict';
 
 module.provider('Intercom', function() {
+  var _asyncLoading = false;
+  var _scriptUrl = '//static.intercomcdn.com/intercom.v1.js';
 
-  this.$get = function() {
-
+  this.asyncLoading = function(config) {
+    _asyncLoading = config || _asyncLoading;
+    return this;
   };
 
+  this.scriptUrl = function(url) {
+    _scriptUrl = url || _scriptUrl;
+    return this;
+  };
+
+  // Create a script tag with moment as the source
+  // and call our onScriptLoad callback when it
+  // has been loaded
+  function createScript($document, callback) {
+    var scriptTag = $document.createElement('script');
+    scriptTag.type = 'text/javascript';
+    scriptTag.async = true;
+    scriptTag.src = _scriptUrl;
+    scriptTag.onreadystatechange = function () {
+      if (this.readyState == 'complete') {
+        callback();
+      }
+    }
+    scriptTag.onload = callback;
+    var s = $document.getElementsByTagName('body')[0];
+        s.appendChild(scriptTag);
+  }
+
+  this.$get = function($q, $rootScope, $window) {
+    var deferred = $q.defer();
+      var _Intercom = $window.Intercom;
+
+      deferred.isPromise = true;
+      _Intercom.isPromise = false;
+
+      if (_asyncLoading) {
+        // Load client in the browser
+        var onScriptLoad = function(callback) {
+          $timeout(function() {
+            deferred.resolve($window.Intercom);
+          });
+        };
+        createScript($document[0], onScriptLoad);
+      }
+
+      return (_asyncLoading) ? deferred.promise: _Intercom;
+  }; // end $get
 })
 
 }(angular.module('angular-intercom',[]),angular, window, document));
