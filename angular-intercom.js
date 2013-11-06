c2e76c1975c3ea34e7906b31d5b275fcfba7b1aa
gdi2290 <github@gdi2290.com>
2013-11-05 20:21:52 -0800

    init angular module for source file

---
+++ angular-intercom.js
@@ -0,0 +1,11 @@
+;(function(module, angular, window, document, undefined) {
+
+module.provider('Intercom', function() {
+
+  this.$get = function() {
+
+  };
+
+})
+
+}(angular.module('angular-intercom',[]),angular, window, document));
