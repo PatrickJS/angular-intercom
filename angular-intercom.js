f0c30b3a669c7059195cfd0c16990890b6eb6e3f
PatrickJS <github@gdi2290.com>
2013-11-27 12:22:26 -0800

    refactor intercom

--- angular-intercom.js
+++ angular-intercom.js
@@ -1,57 +1,35 @@
-;(function(module, angular, window, document, undefined) {
+;(function(module, angular, undefined) {
 'use strict';
 
+angular.module('ngIntercom', ['angular-intercom']);
+
 module.provider('Intercom', function() {
-  var _asyncLoading = false;
-  var _scriptUrl = '//static.intercomcdn.com/intercom.v1.js';
 
-  this.asyncLoading = function(config) {
-    _asyncLoading = config || _asyncLoading;
-    return this;
-  };
+  this.$get = ['$window', 'IntercomSettings', function($window, IntercomSettings) {
+      var _options = {};
+      var _intercom = $window.Intercom;
 
-  this.scriptUrl = function(url) {
-    _scriptUrl = url || _scriptUrl;
-    return this;
-  };
+      angular.extend(_options, IntercomSettings);
 
-  // Create a script tag with moment as the source
-  // and call our onScriptLoad callback when it
-  // has been loaded
-  function createScript($document, callback) {
-    var scriptTag = $document.createElement('script');
-    scriptTag.type = 'text/javascript';
-    scriptTag.async = true;
-    scriptTag.src = _scriptUrl;
-    scriptTag.onreadystatechange = function () {
-      if (this.readyState == 'complete') {
-        callback();
+      return {
+        boot: function(options) {
+          _intercom('boot', _options);
+        },
+        update: function(data) {
+          if (data) _intercom('update', data);
+          else _intercom('update')
+        },
+        shutdown: function() {
+          _intercom('shutdown')
+        },
+        hide: function() {
+          _intercom('hide');
+        },
+        show: function() {
+          _intercom('Show');
+        }
       }
-    }
-    scriptTag.onload = callback;
-    var s = $document.getElementsByTagName('body')[0];
-        s.appendChild(scriptTag);
-  }
-
-  this.$get = function($q, $rootScope, $window) {
-    var deferred = $q.defer();
-      var _Intercom = $window.Intercom;
-
-      deferred.isPromise = true;
-      _Intercom.isPromise = false;
-
-      if (_asyncLoading) {
-        // Load client in the browser
-        var onScriptLoad = function(callback) {
-          $timeout(function() {
-            deferred.resolve($window.Intercom);
-          });
-        };
-        createScript($document[0], onScriptLoad);
-      }
-
-      return (_asyncLoading) ? deferred.promise: _Intercom;
-  }; // end $get
+  }]; // end $get
 })
 
-}(angular.module('angular-intercom',[]),angular, window, document));
+}(angular.module('angular-intercom',[]), angular));
